cbb705750358fb7daa8af3c00c1bba3044b57be2
まの <48278137+Manoruchan@users.noreply.github.com>
2025-03-06 17:23:25 +0900

    Update Enum.js

--- Enum.js
+++ Enum.js
@@ -1,33 +1,3 @@
-/**
- * Enum like class.
- * @example
- * const Week = new Enum(
- *   "MON",
- *   "TUE",
- *   "WED",
- *   "THU",
- *   "FRI",
- *   "SAT",
- *   "SUN"
- * );
- * console.log(Week.MON);
- * >>> 0
- * console.log(Week.THU);
- * >>> 3
- *
- * const Fruits = new Enum(
- *   "APPLE",
- *   ["ORANGE", 10],
- *   "BANANA",
- *   ["MELON", "sweet"]
- * );
- * console.log(Fruits.ORANGE);
- * >>> 10
- * console.log(Fruits.BANANA);
- * >>> 2
- * console.log(Fruits.MELON);
- * >>> "sweet"
- */
 class Enum {
     constructor(...args) {
         args.forEach((v, i) => {
