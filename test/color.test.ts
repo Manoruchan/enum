b499c47149cadea385b3cc06d258439831721802
Manoruchan <yukuanz803@gmail.com>
2025-07-26 01:13:25 +0900

    test: add color example

---
+++ test/color.test.ts
@@ -0,0 +1,14 @@
+import { Enum } from "../src/Enum";
+
+const Color = new Enum(
+    "RED",
+    "GREEN",
+    "BLUE"
+);
+
+console.log(Color.RED);            // 0
+console.log(Color.valueOf('RED')); // 0
+console.log(Color.get(0));         // 'RED'
+console.log(Color.values());       // [ 0, 1, 2 ]
+console.log(Color.names());        // [ 'RED', 'GREEN', 'BLUE' ]
+console.log(Color.toArray());      // [ [ 'RED', 0 ], [ 'GREEN', 1 ], [ 'BLUE', 2 ] ]
