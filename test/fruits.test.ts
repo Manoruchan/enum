3ed2d35e69e7727b7594283e22e960e46570c7e1
Manoruchan <yukuanz803@gmail.com>
2025-07-26 01:19:16 +0900

    style: add single quotation

--- test/fruits.test.ts
+++ test/fruits.test.ts
@@ -7,6 +7,6 @@ const Fruits = new Enum(
 );
 
 console.log(Fruits.APPLE);             // red
-console.log(Fruits.valueOf("ORANGE")); // orange
-console.log(Fruits.get("yellow"));     // BANANA
+console.log(Fruits.valueOf("ORANGE")); // 'orange'
+console.log(Fruits.get("yellow"));     // 'BANANA'
 console.log(Fruits.values());          // [ 'red', 'orange', 'yellow' ]
